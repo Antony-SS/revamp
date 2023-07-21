02e0a40387a3d2149cf5aece85e92ba81178b1af
Antony-SS <Antony@silvettischmitt.net>
2023-07-21 16:18:19 -0500

    preliminary images and border boxes for home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,36 @@
 import React from "react";
-import { Grid, GridItem, Link, Box, Text, Center, Square } from "@chakra-ui/react";
+import { Grid, GridItem, Link, Box, Text, Square, Image, border } from "@chakra-ui/react";
+
 
 function Home() {
   return (
-<Grid h="100vh" templateColumns="2fr 1fr" templateRows="1fr 1fr" gap={0}>
-      <GridItem rowSpan={2} colSpan={1} bg="#90CCF4" p={4}>
+<Grid h="100vh" width= "100%" templateColumns="2fr 1fr" templateRows="1fr 1fr" gap={0}>
+      <GridItem rowSpan={2} colSpan={1} bg="#90CCF4" p={4} position= "relative" >
         <Box as={Link} href="/work" display="block" w="100%" h="100%" padding={0} margin={0}>
           <Square h = "100%">
-            <Image>
-              
-            </Image>
+            <Image src = {require("../resources/desk.png")} alt="DeskSetUp" w= "65vh" h= "65vh"/>
           </Square>
-          <Box position="absolute" bottom="22vh" left={4}>
-              <Text fontSize="sm">Text 1</Text>
+          <Box position="absolute" bottom={0} left={0} right={0} h="25%" bg="rgba(255, 255, 255, 0.7)" display="flex" alignItems="center" justifyContent="center" flexDirection="column" textAlign="center">
+            <Text>Text or Content for the bottom fourth</Text>
           </Box>
         </Box>
       </GridItem>
       {/* </Box> */}
-      <GridItem colSpan={1} bg="#F78888" p={4}>
+      <GridItem colSpan={1} bg="#F78888" p={4} position= "relative">
       <Box as={Link} href="/writing" display="block" w="100%" h="100%" padding={0} margin={0}>
         
-        {/* content here */ }
+        <Box position="absolute" bottom={0} left={0} right={0} h="25%" bg="rgba(255, 255, 255, 0.7)" display="flex" alignItems="center" justifyContent="center" flexDirection="column" textAlign="center">
+              <Text>Text or Content for the bottom fourth</Text>
+        </Box>
+        
       </Box>
         {/* Content for grid item 3 */}
       </GridItem>
-      <GridItem colSpan={1} bg="#F3D250" p={4}>
+      <GridItem colSpan={1} bg="#F3D250" p={4} position="relative">
       <Box as={Link} href="/about" display="block" w="100%" h="100%" padding={0} margin={0}>
-        {/* content here */ }
+        <Box position="absolute" bottom={0} left={0} right={0} h="25%" bg="rgba(255, 255, 255, 0.7)" display="flex" alignItems="center" justifyContent="center" flexDirection="column" textAlign="center">
+                <Text>Text or Content for the bottom fourth</Text>
+        </Box>
       </Box>
       </GridItem>
     </Grid>
