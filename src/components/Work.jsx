4a086d843409f1ca41505292a70257d85f8401b1
Antony-SS <jns5@illinois.edu>
2023-07-26 14:52:36 -0500

    navbar placeholder

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 function Work() {
   return (
-  <Container bg="#90CCF4" minWidth="100%" minHeight="100vh" display="flex" flexDirection="column">
+  <Container bg="#90CCF4" minWidth="100%" minHeight="100vh" display="flex" flexDirection="column" position= {"relative"} margin={0} padding={0}>
+    <Container bg="#000000" minWidth= "100%" minHeight= "10vh" display= "flex" flexDirection= "column" position= "absolute"s>
+
+    </Container>
     <text>WORK</text>
   </Container>
   );
