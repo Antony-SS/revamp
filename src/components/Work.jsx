218441a8ea9eaa9a3b97b128bc436e1ac36ec336
Antony-SS <jns5@illinois.edu>
2023-07-29 00:17:41 -0500

    photshopped desk adding title to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,13 +1,13 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 import React from "react";
 
 function Work() {
   return (
   <Container bg="#90CCF4" minWidth="100%" minHeight="100vh" display="flex" flexDirection="column" position= {"relative"} margin={0} padding={0}>
-    <Container bg="#000000" minWidth= "100%" minHeight= "10vh" display= "flex" flexDirection= "column" position= "absolute"s>
-
+    <Container bg="#000000" minWidth= "100%" minHeight= "10vh" display= "flex" flexDirection= "column" position= "absolute" opacity={.5}/>
+    <Container minWidth= "100%" display= "flex" flexDirection= "column" position = "absolute" textAlign= "center" paddingTop=  "10vh">
+      <Heading fontSize={200} textColor=  "black" opacity={.2}>W O R K</Heading>
     </Container>
-    <text>WORK</text>
   </Container>
   );
 }
