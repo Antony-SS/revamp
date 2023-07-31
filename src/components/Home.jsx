b5c163a5fb5b4f08439bdbce9cd7c8ed91808310
Antony-SS <Antony@silvettischmitt.net>
2023-07-31 10:00:26 -0500

    Smaller subheading, larger heading for elegance

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,30 +4,30 @@ import { Grid, GridItem, Link, Box, Text, Square, Image, Heading } from "@chakra
 
 function Home() {
   return (
-<Grid minHeight="100vh" width= "100%" templateColumns="2fr 1fr" templateRows="1fr 1fr" gap={0}>
+<Grid maxHeight = "100vh" minHeight="100vh" width= "100%" templateColumns="2fr 1fr" templateRows="1fr 1fr" gap={0}>
       <GridItem rowSpan={2} colSpan={1} bg="#90CCF4" p={4} position= "relative" >
         <Box as={Link} href="/work" display="block" w="100%" h="100%" padding={0} margin={0}>
           <Square h = "100%">
-            <Image src = {require("../resources/deskfinal.png")} alt="DeskSetUp" w= "65vh" h= "65vh"/>
+            <Image src = {require("../resources/deskfinal.png")} alt="DeskSetUp" w= "70vh" h= "70vh"/>
           </Square>
-          <Box position="absolute" bottom={0} left={0} right={0} h="25%"  display="flex" textAlign = "left" flexDirection="column" paddingLeft="6%">
-            <Heading fontSize={25}> WORK </Heading>
+          <Box position="absolute" bottom={0} left={0} right={0} h="21%"  display="flex" textAlign = "left" flexDirection="column" paddingLeft="6%">
+            <Heading fontSize={30}> WORK </Heading>
             <Box position="relative" bottom={-2} left={0} w="35px" h="4px" bg="#000000"/>
-            <Text position={"relative"} top={5} fontSize={38}>
+            <Text position={"relative"} top={5} fontSize={16}>
               A few projects I am proud of 
-            <br/>For work, for school, and others just for fun</Text>
+            <br/>for work, for school, and others just for fun</Text>
           </Box>
         </Box>
       </GridItem>
       <GridItem colSpan={1} bg="#F78888" p={4} position= "relative">
         <Box as={Link} href="/writing" display="block" w="100%" h="100%" paddingBottom= "10vh" margin={0}>
-          <Square h="100%">
-            <Image src = {require("../resources/pencil1.png")} h= "38vh"/>
+          <Square h="100%" padding={0}>
+            <Image src = {require("../resources/pencil1.png")} h= "36vh"/>
           </Square>
-          <Box position="absolute" bottom={0} left={0} right={0} h="40%"  display="flex" flexDirection="column" textAlign="left" paddingLeft= "6%">
-            <Heading fontSize={25}> WRITING </Heading>
+          <Box position="absolute" bottom={0} left={0} right={0} h="32%"  display="flex" flexDirection="column" textAlign="left" paddingLeft= "6%">
+            <Heading fontSize={30}> WRITING </Heading>
             <Box position="relative" bottom={-2} left={0} w="35px" h="4px" bg="#000000"/>
-              <Text position={"relative"} top={4} fontSize={25}>
+              <Text position={"relative"} top={4} fontSize={16}>
                 Collection of my technical writing,
               <br/>random thoughts, and developer logs </Text>
           </Box>
@@ -36,12 +36,12 @@ function Home() {
       <GridItem colSpan={1} bg="#F3D250" p={4} position="relative">
       <Box as={Link} href="/about" display="block" w="100%" h="100%" paddingBottom= "10vh" margin={0}>
         <Square h="100%">
-            <Image src = {require("../resources/id.png")} h= "38vh"/>
+            <Image src = {require("../resources/id.png")} h= "36vh"/>
         </Square>
-      <Box position="absolute" bottom={0} left={0} right={0} h="40%" display="flex" flexDirection="column" textAlign="left" paddingLeft= "6%">
-        <Heading fontSize={25}> ABOUT </Heading>
+      <Box position="absolute" bottom={0} left={0} right={0} h="32%" display="flex" flexDirection="column" textAlign="left" paddingLeft= "6%">
+        <Heading fontSize={30}> ABOUT </Heading>
             <Box position="relative" bottom={-2} left={0} w= "35px" h="4px" bg="#000000"/>
-            <Text position={"relative"} top={4} fontSize={25}>
+            <Text position={"relative"} top={4} fontSize={16}>
              Learn a little about me
             <br/>and contact me</Text>
         </Box>
