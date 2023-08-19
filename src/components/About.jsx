f0b2ab79039411264053c5e8d29b1d2227b44b5f
Antony-SS <jns5@illinois.edu>
2023-08-18 20:04:16 -0500

    adding bg and nav to writing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Container, ChakraProvider } from "@chakra-ui/react";
+import { Container, ChakraProvider, VStack } from "@chakra-ui/react";
 import CreateTheme from "../ChakraStyles/CreateTheme"
+import Navbar from "./Shared/Navbar/Navbar";
 
 function About() {
   const theme = CreateTheme("#F3D250");
   return (
     <ChakraProvider theme={theme}>
-      <Container bg="#F3D250" minWidth="100%" minHeight="10vh" display="flex" flexDirection="column">
-        <text>ABOUT</text>
-      </Container>
+      <VStack>
+        <Navbar backgroundColor={"#F3D250"}/>
+        <Container bg="#F3D250" paddingTop = "20vh" minWidth="100%" minHeight="10vh" display="flex" flexDirection="column">
+          <text>ABOUT</text>
+        </Container>
+      </VStack>
     </ChakraProvider>
   );
 }
--- src/components/Writing.jsx
+++ src/components/Writing.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { ChakraProvider, Container, VStack, Box, Heading } from "@chakra-ui/react";
 import CreateTheme from "../ChakraStyles/CreateTheme";
+import Navbar from "./Shared/Navbar/Navbar";
 
 function Writing(bg) {
   const theme = CreateTheme("#F78888");
   return (
     <ChakraProvider theme = {theme}>
-      <Container bg="#F78888" minWidth="100%" minHeight="100vh" display="flex" flexDirection="column">
-        <text>WRITING</text>
-      </Container>
+      <VStack>
+        <Navbar backgroundColor={"#F78888"}/>
+        <Container minWidth = "100%" display= "flex" flexDirection= "column" top = "15vh" paddingTop={"10vh"} position = "relative" textAlign= "center" alignItems={"center"} justifyContent={"center"} margin={0}>
+          <Box border="4px solid rgba(0, 0, 0, 0.2)" borderRadius="10px" padding={0} margin={0} textAlign={"center"} justifyContent={"center"}>
+        <Heading paddingLeft={10} paddingRight={10} paddingBottom={4} fontSize={240} textColor= "black" opacity={.15}>WRITING</Heading>
+      </Box>
+    </Container>
+      </VStack>
     </ChakraProvider>
   );
 }
